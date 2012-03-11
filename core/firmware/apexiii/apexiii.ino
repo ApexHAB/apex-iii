f5765b44966ee93faa2755d23878b87427c1ed00
Priyesh Patel <priyesh@ukawala.com>
2012-03-11 00:53:53 +0000

    core transmits last known gps data rather than ,,,,, - ready for L2

--- core/firmware/apexiii/apexiii.ino
+++ core/firmware/apexiii/apexiii.ino
@@ -25,14 +25,16 @@
 
 // Define settings
 uint8_t STATUS_LED_PIN = 13;
-uint8_t EXT_TEMP_ADDR[8] = {0x28, 0x20, 0x38, 0xBB, 0x03, 0x00, 0x00, 0xF3};
-uint8_t INT_TEMP_ADDR[8] = {0x28, 0xEF, 0xC6, 0x5E, 0x03, 0x00, 0x00, 0x84};
+uint8_t EXT_TEMP_ADDR[8] = {0x28, 0x8D, 0x0C, 0x68, 0x03, 0x00, 0x00, 0xF7};
+uint8_t INT_TEMP_ADDR[8] = {0x28, 0x42, 0x99, 0x5E, 0x03, 0x00, 0x00, 0x68};
 char SD_LOG_FILENAME[] = "APEXIII.LOG";
 char SD_GPS_FILENAME[] = "GPS.LOG";
 uint8_t SD_CS = 10;
 
 // Define packet variable
 char packet[200];
+// Last GPS string with fix
+char gps_lastfix[65];
 // Current altitude
 uint16_t altitude = 0;
 // Current command
@@ -61,6 +63,9 @@ void setup()
     pinMode(SD_CS, OUTPUT);
     if (!SD.begin(SD_CS)) Serial.println("SD card failed to initialise");
 
+    // Initialise GPS last fix string 
+    gps_get(gps_lastfix);
+
     // System booted
     Serial.println("");
     Serial.println("Apex III successfully booted");
@@ -81,7 +86,7 @@ void loop()
     counter_inc();
 
     // Get altitude
-    altitude = gps_altitude();
+    // altitude = gps_altitude();
 
     // Get data from external sensors and devices and then construct the packet
     build_packet();
@@ -90,7 +95,7 @@ void loop()
     rtty_prepare(packet);
 
     // Print packet to serial
-    Serial.print(packet);
+    // * // Serial.print(packet);
 
     // Write packet to SD card
     sdcard_log(packet); 
@@ -98,11 +103,11 @@ void loop()
     sdcard_gps();
 
     // Telemetry
-    Serial.print("Telemetry started... ");
+    // * // Serial.print("Telemetry started... ");
 
-    delay(100);
+    // * // delay(100);
 
-    sdcard_gps();
+    // * // sdcard_gps();
 
     // Count down the packet type
     packet_type -= 1;
@@ -125,18 +130,18 @@ void loop()
         rtty_tx(packet, 0);
     }
 
-    Serial.println("finished");
-    
-    sdcard_gps();
+    // * // Serial.println("finished");
+    // * // 
+    // * // sdcard_gps();
 
-    // Delay until the next packet
-    // This window is also for UART commands to be entered in
-    delay(2000);
-    sdcard_gps();
-    delay(2000);
+    // * // // Delay until the next packet
+    // * // // This window is also for UART commands to be entered in
+    // * // delay(2000);
+    // * // sdcard_gps();
+    // * // delay(2000);
 
-    // Check for any inputted UART commands
-    uart_commands();
+    // * // // Check for any inputted UART commands
+    // * // uart_commands();
 }
 
 /**
@@ -144,9 +149,9 @@ void loop()
  */
 void build_packet()
 {
-    // Broadcast to modules
-    modules_broadcast(altitude, command);
-    command = NONE;
+    // * // // Broadcast to modules
+    // * // modules_broadcast(altitude, command);
+    // * // command = NONE;
 
     // External temperature sensor
     char external_temp[10];
@@ -162,37 +167,46 @@ void build_packet()
     // GPS 
     char gps_data[65];
     gps_get(gps_data); 
+    // If no fix, return last fix
+    if (strcmp(gps_data, ",,,,,") == 0)
+    {
+        strcpy(gps_data, gps_lastfix);
+    }
+    else
+    {
+        strcpy(gps_lastfix, gps_data);
+    }
 
     // Reminder - bytes from modules may need casting as int/uint
 
-    // Light A
-    char light_a[MODULES_BUFFER_LENGTH];
-    modules_request(light_a, LGTA, altitude);
-    
-    // Gas 
-    char gas[MODULES_BUFFER_LENGTH];
-    modules_request(gas, GASS, altitude);
+    //// Light A
+    //char light_a[MODULES_BUFFER_LENGTH];
+    //modules_request(light_a, LGTA, altitude);
+    //
+    //// Gas 
+    //char gas[MODULES_BUFFER_LENGTH];
+    //modules_request(gas, GASS, altitude);
 
-    // Light B
-    char light_b[MODULES_BUFFER_LENGTH];
-    modules_request(light_b, LGTB, altitude);
-    uint16_t light_red = (light_a[3] << 8) | (light_a[2]);
-    uint16_t light_green = (light_a[5] << 8) | (light_a[4]);
-    uint16_t light_blue = (light_b[3] << 8) | (light_b[2]);
-    uint16_t light_normal = (light_b[5] << 8) | (light_b[4]);
+    //// Light B
+    //char light_b[MODULES_BUFFER_LENGTH];
+    //modules_request(light_b, LGTB, altitude);
+    //uint16_t light_red = (light_a[3] << 8) | (light_a[2]);
+    //uint16_t light_green = (light_a[5] << 8) | (light_a[4]);
+    //uint16_t light_blue = (light_b[3] << 8) | (light_b[2]);
+    //uint16_t light_normal = (light_b[5] << 8) | (light_b[4]);
 
     // Build the packet
-    sprintf(packet,"$$APEX,%u,%s,%s,%s,%u,%u,%u,%u,%u,%u,%s",
+    sprintf(packet,"$$APEX,%u,%s,%s,%s,%s",
         counter_get(),
         gps_data,
         external_temp,
         internal_temp,
-        light_normal,
-        light_red,
-        light_green,
-        light_blue,
-        (uint8_t) gas[2],
-        (uint8_t) gas[3],
+        //light_normal,
+        //light_red,
+        //light_green,
+        //light_blue,
+        //(uint8_t) gas[2],
+        //(uint8_t) gas[3],
         battery_voltage
     );
 }
