d20aafb7e8a4e77daeca6c9a68f955517f91221d
Priyesh Patel <priyesh@ukawala.com>
2012-03-18 22:31:56 +0000

    core firmware cleaned up

--- core/firmware/apexiii/apexiii.ino
+++ core/firmware/apexiii/apexiii.ino
@@ -86,7 +86,7 @@ void loop()
     counter_inc();
 
     // Get altitude
-    // altitude = gps_altitude();
+    altitude = gps_altitude();
 
     // Get data from external sensors and devices and then construct the packet
     build_packet();
@@ -95,7 +95,7 @@ void loop()
     rtty_prepare(packet);
 
     // Print packet to serial
-    // * // Serial.print(packet);
+    Serial.print(packet);
 
     // Write packet to SD card
     sdcard_log(packet); 
@@ -103,11 +103,11 @@ void loop()
     sdcard_gps();
 
     // Telemetry
-    // * // Serial.print("Telemetry started... ");
+    Serial.print("Telemetry started... ");
 
-    // * // delay(100);
+    delay(100);
 
-    // * // sdcard_gps();
+    sdcard_gps();
 
     // Count down the packet type
     packet_type -= 1;
@@ -130,18 +130,18 @@ void loop()
         rtty_tx(packet, 0);
     }
 
-    // * // Serial.println("finished");
-    // * // 
-    // * // sdcard_gps();
+    Serial.println("finished");
+    
+    sdcard_gps();
 
-    // * // // Delay until the next packet
-    // * // // This window is also for UART commands to be entered in
-    // * // delay(2000);
-    // * // sdcard_gps();
-    // * // delay(2000);
+    // Delay until the next packet
+    // This window is also for UART commands to be entered in
+    delay(2000);
+    sdcard_gps();
+    delay(2000);
 
-    // * // // Check for any inputted UART commands
-    // * // uart_commands();
+    // Check for any inputted UART commands
+    uart_commands();
 }
 
 /**
@@ -149,9 +149,9 @@ void loop()
  */
 void build_packet()
 {
-    // * // // Broadcast to modules
-    // * // modules_broadcast(altitude, command);
-    // * // command = NONE;
+    // Broadcast to modules
+    modules_broadcast(altitude, command);
+    command = NONE;
 
     // External temperature sensor
     char external_temp[10];
@@ -179,34 +179,34 @@ void build_packet()
 
     // Reminder - bytes from modules may need casting as int/uint
 
-    //// Light A
-    //char light_a[MODULES_BUFFER_LENGTH];
-    //modules_request(light_a, LGTA, altitude);
-    //
-    //// Gas 
-    //char gas[MODULES_BUFFER_LENGTH];
-    //modules_request(gas, GASS, altitude);
+    // Light A
+    char light_a[MODULES_BUFFER_LENGTH];
+    modules_request(light_a, LGTA, altitude);
+    
+    // Gas 
+    char gas[MODULES_BUFFER_LENGTH];
+    modules_request(gas, GASS, altitude);
 
-    //// Light B
-    //char light_b[MODULES_BUFFER_LENGTH];
-    //modules_request(light_b, LGTB, altitude);
-    //uint16_t light_red = (light_a[3] << 8) | (light_a[2]);
-    //uint16_t light_green = (light_a[5] << 8) | (light_a[4]);
-    //uint16_t light_blue = (light_b[3] << 8) | (light_b[2]);
-    //uint16_t light_normal = (light_b[5] << 8) | (light_b[4]);
+    // Light B
+    char light_b[MODULES_BUFFER_LENGTH];
+    modules_request(light_b, LGTB, altitude);
+    uint16_t light_red = (light_a[3] << 8) | (light_a[2]);
+    uint16_t light_green = (light_a[5] << 8) | (light_a[4]);
+    uint16_t light_blue = (light_b[3] << 8) | (light_b[2]);
+    uint16_t light_normal = (light_b[5] << 8) | (light_b[4]);
 
     // Build the packet
-    sprintf(packet,"$$APEX,%u,%s,%s,%s,%s",
+    sprintf(packet,"$$APEX,%u,%s,%s,%s,%u,%u,%u,%u,%u,%u,%s",
         counter_get(),
         gps_data,
         external_temp,
         internal_temp,
-        //light_normal,
-        //light_red,
-        //light_green,
-        //light_blue,
-        //(uint8_t) gas[2],
-        //(uint8_t) gas[3],
+        light_normal,
+        light_red,
+        light_green,
+        light_blue,
+        (uint8_t) gas[2],
+        (uint8_t) gas[3],
         battery_voltage
     );
 }
